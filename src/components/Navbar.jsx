e0eabcc2a9e215cdf01a25b42b475930c3c4dfbe
AntsoTech <antso-rakotoson@hotmail.fr>
2022-07-16 02:51:18 +0200

    [routes]routes and links are implemented to navigate between all urls

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import "./navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 	return (
 		<nav className="navbar">
-			<img
-				src="https://upload.wikimedia.org/wikipedia/fr/thumb/6/65/Football_Portugal_federation.svg/1200px-Football_Portugal_federation.svg.png"
-				alt="Logo"
-				width="24px"
-				height="32px"
-			/>
+			<Link to="/" style={{ textDecoration: "none" }}>
+				<img
+					src="https://upload.wikimedia.org/wikipedia/fr/thumb/6/65/Football_Portugal_federation.svg/1200px-Football_Portugal_federation.svg.png"
+					alt="Logo"
+					width="24px"
+					height="32px"
+				/>
+			</Link>
 			<ul className="navbar__categories">
-				<li> Funzone </li>
-				<li> Testimoniales </li>
+				<Link to="/funzone" style={{ textDecoration: "none" }}>
+					<li> Funzone </li>
+				</Link>
+				<Link to="/testimoniales" style={{ textDecoration: "none" }}>
+					<li> Testimoniales </li>
+				</Link>
 			</ul>
 			<ul className="navbar__buttons">
 				<img src="../assets/user.png" alt="user" width="20px" height="20px" />
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 import {
 	ApolloClient,
 	InMemoryCache,
@@ -17,7 +18,7 @@ const link = new HttpLink({
 	},
 });
 
-// Defining cache type policies to enable merges when quering again
+// Defining cache type policies to enable merging when quering again
 const cache = new InMemoryCache({
 	typePolicies: {
 		Query: {
@@ -31,16 +32,13 @@ const cache = new InMemoryCache({
 						// If we couldn't find the cursor, default to appending to
 						// the end of the list, so we don't lose any data.
 						if (offset < 0) offset = merged.length;
-						// Now that we have a reliable offset, the rest of this logic
-						// is the same as in offsetLimitPagination.
 						for (let i = 0; i < incoming.length; ++i) {
 							merged[offset + i] = incoming[i];
 						}
 						return merged;
 					},
 
-					// If you always want to return the whole list, you can omit
-					// this read function.
+					// If we dont want to return the whole list
 					read(
 						existing,
 						{ args: { cursor, limit = existing.length }, readField }
@@ -59,16 +57,11 @@ const cache = new InMemoryCache({
 	},
 });
 
+// Function to get the offset from a cursor
 function offsetFromCursor(items, cursor, readField) {
-	// Search from the back of the list because the cursor we're
-	// looking for is typically the ID of the last item.
+	// We search from the back of the list because cursor = ID of the last item.
 	for (let i = items.length - 1; i >= 0; --i) {
 		const item = items[i];
-		// Using readField works for both non-normalized objects
-		// (returning item.id) and normalized references (returning
-		// the id field from the referenced entity object), so it's
-		// a good idea to use readField when you're not sure what
-		// kind of elements you're dealing with.
 		if (readField("id", item) === cursor) {
 			// Add one because the cursor identifies the item just
 			// before the first item in the page we care about.
@@ -86,7 +79,9 @@ const client = new ApolloClient({
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+	<HashRouter>
 	<ApolloProvider client={client}>
 		<App />
 	</ApolloProvider>
+	</HashRouter>
 );
